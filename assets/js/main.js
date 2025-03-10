dddebdaf4502195f99cbc44a24c1545af0d63c27
bingxue <bingxue1114@gmail.com>
2025-03-10 18:02:20 +0800

    v3.1 新增手機禁止下載

--- assets/js/main.js
+++ assets/js/main.js
@@ -357,7 +357,6 @@
                         .overlay { 
                             position: absolute; 
                             top: 0; left: 0; width: 100%; height: 100%; 
-                            background: rgba(0, 0, 0, 0.2); 
                             z-index: 1000; 
                         }
                         @media print {
